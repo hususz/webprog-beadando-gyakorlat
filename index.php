b45cf18a6d0ff3629405facea901abf0b5f7de3e
Hüse Imre <huseimi20010903@gmail.com>
2026-05-02 10:21:12 +0200

    hiibakeresés

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include('./includes/config.inc.php');
 
 $oldal = isset($_GET['oldal']) ? $_GET['oldal'] : '';
 
 if ($oldal != "") {
-    if (isset($oldalak[$oldal]) && file_exists("./templates/pages/{$oldalak[$oldal]['fajl']}.tpl.php")) {
-        $keres = $oldalak[$oldal];
-    } else {
+    if (!isset($oldalak[$oldal])) {
+        echo "<div style='background:red; color:white; padding:10px;'>HIBA: A(z) '$oldal' nincs benne a config.inc.php tömbjében!</div>";
         $keres = $hiba_oldal;
-        header("HTTP/1.0 404 Not Found");
+    } elseif (!file_exists("./templates/pages/{$oldalak[$oldal]['fajl']}.tpl.php")) {
+        echo "<div style='background:red; color:white; padding:10px;'>HIBA: A sablon fájl nem létezik: ./templates/pages/{$oldalak[$oldal]['fajl']}.tpl.php</div>";
+        $keres = $hiba_oldal;
+    } else {
+        $keres = $oldalak[$oldal];
     }
 } else {
     $keres = $oldalak['/'];
